3d8b167bce0ecf236c69360843f8d758bf5890cd
gabriel-acesso <gabriel.pereira@acessoenergia.com.br>
2025-08-21 22:47:16 -0300

    fix deploy

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,2 +1,6 @@
 // Export all shared hooks from here
+export { useDebounce } from "./useDebounce";
 export { useDelayedLoading } from "./useDelayedLoading";
+export { useLoadingConfig } from "./useLoadingConfig";
+export { useTheme } from "./useTheme";
+export { useToast } from "./useToast";
