51d064d60cba5f77afe47a7ba3e4325ca9417ebf
gabriel-acesso <gabriel.pereira@acessoenergia.com.br>
2025-09-05 09:11:28 -0300

    feat: update project configuration for Node.js 20 and pnpm compatibility

--- src/shared/components/layout/index.ts
+++ src/shared/components/layout/index.ts
@@ -1,18 +1,8 @@
 export { Breadcrumb } from "./Breadcrumb";
 export { MainLayout } from "./MainLayout";
-export { TopBar } from "./topbar";
 
-// Novo TopBar modular
-export { TopBar as TopBarNew } from "./topbar";
-export { TopBarNotifications } from "./topbar/TopBarNotifications";
-export { TopBarSearch } from "./topbar/TopBarSearch";
-export { TopBarSidebarToggle } from "./topbar/TopBarSidebarToggle";
-export { TopBarUserMenu } from "./topbar/TopBarUserMenu";
+// TopBar - importa do index do topbar
+export * from "./topbar";
 
-// Sidebar
-export { Sidebar } from "./sidebar";
-export { SidebarFooter } from "./sidebar/SidebarFooter";
-export { SidebarHeader } from "./sidebar/SidebarHeader";
-export { SidebarItem } from "./sidebar/SidebarItem";
-export { SidebarNavigation } from "./sidebar/SidebarNavigation";
-export { SidebarSection } from "./sidebar/SidebarSection";
+// Sidebar - importa do index do sidebar
+export * from "./sidebar";
